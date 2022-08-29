05deaf44e839fff3b460d60b2f12f87bcd4ee858
SyfaCollins <coniplexy@gmail.com>
2022-08-29 22:14:00 +0300

    task manager

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -24,9 +24,9 @@ const updateTask = (req, res) => {
   res.send("update a task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete a task");
-};
+// const deleteTask = (req, res) => {
+//   res.send("delete a task");
+// };
 
 module.exports = {
   getAllTasks,
